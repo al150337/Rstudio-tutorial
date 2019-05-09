0319567b8948018dd282d061df5825f2f5e94a54
Luis Angel <al150337@alumnos.uacj.mx>
2019-05-09 16:37:31 -0600

    UPDATE 09-05-2019

---
+++ script tutorial.R
@@ -0,0 +1,32 @@
+#Arithmetic
+2^5
+28%%6
+#Comentarios
+#Variable
+x<-42
+print(x)
+my_apples<-5
+my_apples
+my_oranges<-6
+my_apples+my_oranges
+my_fruit<-my_apples+my_oranges
+my_fruit
+#Data types
+my_numeric<-42
+my_character<-"universe"
+my_logical<-FALSE
+class(my_numeric)
+class(my_character)
+class(my_logical)
+#Vectors
+vegas<-"Go!"
+numeric_vector<-c(1,2,3)
+character_vector<-c("a","b","c")
+boolean_vector<-c(TRUE, FALSE, TRUE)
+poker_vector<-c(140,-50,20,-120,240)
+roulette_vector<-c(-24,-50,100,-350,10)
+names(poker_vector)<-c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(roulette_vector)<-c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+total_daily<-poker_vector+roulette_vector
+total_daily
+
